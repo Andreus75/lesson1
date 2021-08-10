68375b15113f0f9fc155d0461cb8f3034a6d260e
Andreus75 <t_andriy@ukr.net>
2021-08-10 14:02:49 +0300

    finel version

--- src/components/simpson/Simpson.js
+++ src/components/simpson/Simpson.js
@@ -2,8 +2,8 @@ import "./simpson.css"
 export function Simpson(props) {
     let {name, surname, age, info, photo} = props;
     return (
-            <div className={'simpson'} alt="">
-                <img src={photo}/>
+            <div className={'simpson'}>
+                <img src={photo} alt=""/>
                 <p3>{name} {surname} - {age} years</p3>
                 <h4>{info}</h4>
             </div>
